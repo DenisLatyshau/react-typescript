1b10e54e8946489a91e0614f15d82b027ace3b16
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-31 15:39:30 +0300

    feat: add List.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+import List from './components/List';
+import { ITodo, IUser } from './types/types';
 import axios from 'axios';
+import UserItem from './components/UserItem';
 
 const App = () => {
   const [users, setUsers] = useState<IUser[]>([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   useEffect(() => {
     fetchUsers()
+    fetchTodos()
   }, [])
 
   async function fetchUsers() {
@@ -20,12 +23,24 @@ const App = () => {
     }
   }
 
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      setTodos(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
   return (
     <div>
       <Card variant={CardVariant.outlined} width='200px' height='200px'>
         <button>Кнопка</button>
       </Card>
-      <UserList users={users} />
+      <List 
+          items={users} 
+          renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
+      />
     </div>
   );
 }
